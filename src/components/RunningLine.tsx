8ed157327f24001e368d78a92a5dbe1b61fd7c44
Vitalii-250885 <provitaleks@gmail.com>
2024-11-13 15:43:58 +0200

    Update RunningLine.tsx

--- src/components/RunningLine.tsx
+++ src/components/RunningLine.tsx
@@ -3,14 +3,14 @@ import { useLayoutEffect, useRef } from 'react'
 
 const RunningLine = () => {
 	const textRef = useRef(null)
-	const spanRef = useRef(null)
+	const spanRef = useRef<HTMLSpanElement | null>(null)
 
 	useLayoutEffect(() => {
 		gsap.fromTo(
 			textRef.current,
 			{ x: 0 },
 			{
-				x: -spanRef.current.clientWidth + 'px',
+				x: -spanRef.current!.clientWidth + 'px',
 				duration: 5,
 				repeat: -1,
 				ease: 'none',
@@ -29,7 +29,7 @@ const RunningLine = () => {
 	return (
 		<div className='flex w-full items-center overflow-hidden h-32 mt-4 md:mt-9 md:h-48 xl:mt-44 2xl:h-64 2xl:mt-28'>
 			<div ref={textRef} className='flex items-center'>
-				{[...Array(4)].map((_, i) => (
+				{[...Array(6)].map((_, i) => (
 					<SlideElement key={i} />
 				))}
 			</div>

6a5975fd65a288d177b7e9174bd56865c79fbc83
Vitalii-250885 <provitaleks@gmail.com>
2024-11-13 12:26:05 +0200

    add markup

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+function Header() {
+	return (
+		<div className='pt-10 px-6 flex flex-col items-center'>
+			<img src='Logo.svg' alt='Skilldor' className='w-20' />
+			<h1 className='mt-10'>Your Basketball Journey Starts Here</h1>
+			<span>Train. Improve. Reward Yourself</span>
+			<img src='BasketballBall.png' alt='Basketball ball' />
+		</div>
+	)
+}
+
+export default Header

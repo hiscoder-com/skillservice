f1930c4b73da1729536cb523168c0a4279ab6e2b
Vitalii-250885 <provitaleks@gmail.com>
2025-01-27 15:12:09 +0200

    Added contact form

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom'
 function Layout() {
 	return (
 		<>
-			<header className='h-24 bg-gradient-end pl-32 pt-3 fill-white text-white'>
+			<header className='h-24 bg-primary-600 pl-10 md:pl-32 pt-3 fill-white text-white'>
 				<img src='/Logo-white.svg' alt='Logo' className='w-20' />
 			</header>
-			<main className='px-6 py-3'>
+			<main className='px-3 py-3 md:px-6 md:py-10'>
 				<Outlet />
 			</main>
 		</>

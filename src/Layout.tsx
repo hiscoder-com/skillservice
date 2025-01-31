e3a9fad7976f6b12a7f3affbed411900fc927b8a
Vitalii-250885 <provitaleks@gmail.com>
2025-01-31 14:59:50 +0200

    Update Layout.tsx

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,16 +4,16 @@ import CookiesBanner from './components/CookiesBanner'
 
 function Layout() {
   return (
-    <>
+    <div className='h-full'>
       <header className='h-24 bg-primary-600 pl-10 md:pl-32 pt-3 fill-white text-white'>
         <img src='/Logo-white.svg' alt='Logo' className='w-20' />
       </header>
-      <main className='px-3 py-3 md:px-6 md:py-10'>
+      <main className='px-3 py-3 md:px-6 md:py-10 max-w-[120rem] mx-auto'>
         <Outlet />
       </main>
       <CookiesBanner />
-      <footer className='h-12 bg-primary-600'></footer>
-    </>
+      {/* <footer className='h-12 bg-primary-600'></footer> */}
+    </div>
   )
 }
 

1ae48f1cc3242c85db2e667c7d73eb5b491c0b6f
Vitalii-250885 <provitaleks@gmail.com>
2025-02-03 16:11:47 +0200

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -7,12 +7,12 @@ function Title({ children, type }: TitleProps) {
   return (
     <>
       {type === 'h2' && (
-        <h2 className='text-2xl font-semibold ml-10 text-primary-500 sm:text-3xl'>
+        <h2 className='text-2xl font-semibold text-primary-500 sm:text-3xl'>
           {children}
         </h2>
       )}
       {type === 'h3' && (
-        <h3 className='text-xl font-semibold ml-6 mt-6 text-primary-500 sm:text-2xl sm:mt-12'>
+        <h3 className='text-xl font-semibold mt-6 text-primary-500 sm:text-2xl sm:mt-12'>
           {children}
         </h3>
       )}

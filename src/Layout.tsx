892869814784aa8619ef648428d5782e0818f04c
Vitalii-250885 <provitaleks@gmail.com>
2025-02-03 16:12:54 +0200

    Update Layout.tsx

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,7 +6,9 @@ function Layout() {
   return (
     <div className='h-full'>
       <header className='h-24 bg-primary-600 pl-10 md:pl-32 pt-3 fill-white text-white'>
-        <img src='/logo-white.svg' alt='Logo' className='w-20' />
+        <a href='/'>
+          <img src='/logo-white.svg' alt='Logo' className='w-20' />
+        </a>
       </header>
       <main className='px-3 py-3 md:px-6 md:py-10 max-w-[120rem] mx-auto'>
         <Outlet />
